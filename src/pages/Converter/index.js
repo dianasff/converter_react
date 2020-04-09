72c5e210fed3f3973ec0e0c144b145f8b1bb9375
Diana Felix <dianasf.f@gmail.com>
2020-04-09 16:44:34 -0300

    Change logic on form submit

--- src/pages/Converter/index.js
+++ src/pages/Converter/index.js
@@ -1,43 +1,55 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import {useHistory}  from 'react-router-dom';
 import './styles.css';
 import api from '../../service/api';
+import logoimg from '../../assets/logo_c.png'
 
 
 export default function Converter(){
     const [value, setValue]=useState('');
     const [tax, setTax]= useState('');
     const [type, setType] =useState('');
+    const [quotation, setQuotation] =useState('');
+    
+    
     
     const history=useHistory();
     
+    useEffect(()=>{
+        api.get().then(response =>{
+            setQuotation(Number.parseFloat(response.data.USD.ask).toFixed(2));
+        })
     
+    })
     async function handleSubmit(evt){
         evt.preventDefault();
         try{
             const response = await api.get();
             console.log(response.data.USD.ask);
+            
+            const calculate=(number_tax)=>{
+                const total_dolar_notax = Number.parseFloat(value);
+                const total_dolar_tax   =(total_dolar_notax + (total_dolar_notax*(Number.parseFloat(tax)/100)));
+                const total_reais_notax =(total_dolar_tax)* Number(response.data.USD.ask);
+                const total_reais_tax   =((total_reais_notax * number_tax) + total_reais_notax );
+                localStorage.setItem('quotation', Number.parseFloat(response.data.USD.ask).toFixed(2));
+                localStorage.setItem('total_dolar_notax', Number.parseFloat(total_dolar_notax).toFixed(2));
+                localStorage.setItem('total_dolar_tax', Number.parseFloat(total_dolar_tax).toFixed(2));
+                localStorage.setItem('total_reais_notax',Number.parseFloat(total_reais_notax).toFixed(2));
+                localStorage.setItem('total_reais_tax', Number.parseFloat(total_reais_tax).toFixed(2));
+                
+            }
+            
             if(type === 'money'){  
-                const iof= '1.1%';  
-                const total_dolar_notax= [value + (value*tax)] ;
-                const total_dolar_tax= [value + (value*tax)]*0.011 ;
-                const total_reais_tax= ([value + (value*tax)]*0.011)*5.05 ;
-                localStorage.setItem('quotation', response.data.USD.ask);
-                localStorage.setItem('total_dolar_notax', total_dolar_notax);
-                localStorage.setItem('total_dolar_notax', total_dolar_notax);
-                localStorage.setItem('total_dolar_tax', total_dolar_tax);
-                localStorage.setItem('total_reais_tax', total_reais_tax);
+                const iof= '1.1'; 
                 localStorage.setItem('iof', iof);
+                calculate(0.011)
+                
             }
             if(type === 'card'){  
-                const iof= '6.4%';  
-                const total_dolar_notax= [value + (value*tax)] ;
-                const total_dolar_tax= [value + (value*tax)]*0.064 ;
-                const total_reais_tax= ([value + (value*tax)]*0.064)*5.05 ;
-                localStorage.setItem('total_dolar_notax', total_dolar_notax);
-                localStorage.setItem('total_dolar_tax', total_dolar_tax);
-                localStorage.setItem('total_reais_tax', total_reais_tax);
+                const iof= '6.4';
                 localStorage.setItem('iof', iof);
+                calculate(0.064)
             }      
             history.push('/results');
         }catch(err){
@@ -51,41 +63,55 @@ export default function Converter(){
         <div className='principal'>
             <div className='content'>
                 <section>
-                    
-                    <h1>Converter App</h1>
+                    <img src={logoimg} alt=' logo Converter App' />
                     <p>Esse é o Converter App, sua aplicação para conversão de valores de Dólar para Real!</p>
                     <h2>Instruções:</h2>
                     <ul>
-                        <li>Insira o valor , em dolar, que deseja converter</li>
-                        <li>Insira a taxa do estado </li>
+                        <li> 1º Insira o valor , em dolar, que deseja converter</li>
+                        <li> 2º Insira a taxa do estado </li>
+                        <li><h3>Valor do dólar hoje: R$   {quotation} </h3></li>
                     </ul>
                     
                 </section>
-                <form >
+                <form onSubmit={handleSubmit} >
+                    
                     <input  
-                        placeholder='Valor em dólar' required                    
+                        placeholder='Valor em dólar' required="required"
+                        id='valor'
+                        type='text'
+                        maxLength='12'
+                        minLength='1'
+                        pattern="\d+(\.\d{2})?" 
+                        title='Não é permitido o uso de sinais (+) ou (-); Use números e ponto(.) para separar decimal. ' 
                         value={value}
-                        onChange={evt=> setValue(evt.target.value)}
+                        onChange={evt=> setValue((evt.target.value))}
                     />
+                    
                     <input  
-                        placeholder='Taxa do estado' required  
+                        placeholder='Taxa do estado' required="required"  
+                        type='text'
+                        id='taxa'
+                        maxLength='4'
+                        minLength='1'
+                        pattern="\d{1}\.\d{2}" 
+                        title='Não é permitido o uso de sinais (+) ou (-); Use números e ponto(.) para separar decimal. '
                         value={tax}
-                        onChange={evt=> setTax(evt.target.value)}
+                        onChange={evt=> setTax((evt.target.value))}
                     />
                     <div className='radio'>
                         <label htmlFor='money'>Dinheiro:</label>
                         <input 
-                            type='radio' id='money' name='payment_type' value='money' required
+                            type='radio' id='money' name='payment_type' value='money' required="required"
                             onChange={evt=> setType(evt.target.value)}
                         />
                         <label htmlFor='card'>Cartão:</label>
                         <input 
-                            type='radio' id='card' name='payment_type' value='card' required
+                            type='radio' id='card' name='payment_type' value='card' required="required"
                             onChange={evt=> setType(evt.target.value)}
                         />
                     </div>
                     
-                        <button onClick={handleSubmit} className='button'>
+                        <button   type='submit' className='button'>
                             Converter
                         </button>
                     
--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,14 +1,26 @@
-import React  from 'react';
-import {Link}  from 'react-router-dom';
+import React from 'react';
+import {useHistory}  from 'react-router-dom';
 import './styles.css';
+import logoimg from '../../assets/logo_c.png'
 
 
 export default function Results(){
+    
+
     const quotation =   localStorage.getItem('quotation');
     const iof =   localStorage.getItem('iof');
-    const total_dolar_notax = localStorage.getItem('total_dolar_notax');
-    const total_dolar_tax= localStorage.getItem('total_dolar_tax');
-    const total_reais_tax = localStorage.getItem('total_reais_tax');
+    const total_dolar_notax =   localStorage.getItem('total_dolar_notax');
+    const total_dolar_tax =   localStorage.getItem('total_dolar_tax');
+    const total_reais_notax =   localStorage.getItem('total_reais_notax');
+    const total_reais_tax =   localStorage.getItem('total_reais_tax');
+    const history= useHistory();
+    
+    function handleSubmit(){
+        localStorage.clear();
+        history.push('/')
+    
+
+    }
     
 
     return(
@@ -16,27 +28,27 @@ export default function Results(){
             <div className='content'>
                 <section className='presentation'>
                     
-                    <h1>Converter App</h1>
+                    <img src={logoimg} alt='Logo converter app'/>
                     <p>Esse é o Converter App, sua aplicação para conversão de valores de Dólar para Real!</p>
-                    <Link className='back' to='/'>
-                        Voltar
-                    </Link>
+                    
                     
                 </section>
                 <section className='results'>
                     <h1>RESULTADOS:</h1>
                     <ul>
-                        <li>Cotação em dólar:{quotation} </li>
-                        <li>IOF:{iof} </li>
-                        <li>Total em dólar sem imposto:{total_dolar_notax} </li>
-                        <li>Total em dólar com imposto:{total_dolar_tax} </li>
-                        <li>Total em reais com imposto:{total_reais_tax} </li>
+                        <li>Cotação em dólar: R$  {quotation} </li>
+                        <li>IOF:    {iof}% </li>
+                        <li>Total em dólar sem imposto: U$$ {total_dolar_notax} </li> 
+                        <li>Total em dólar com imposto: U$$ {total_dolar_tax} </li>
+                        <li>Total em reais sem imposto: R$  {total_reais_notax} </li>
+                        <li>Total em reais com imposto: R$ {total_reais_tax} </li> 
+      
                     </ul>
-                    <Link className='back' to='/'>
-                        <button className='button'>
+                    
+                        <button className='button' onClick={handleSubmit}>
                             Fazer uma nova consulta
                         </button>
-                    </Link>
+                    
                     
                 </section>
             </div>
