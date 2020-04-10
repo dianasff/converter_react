604d74a73bf75714e585a95ec6d6e9b235cb6f38
Diana Felix <dianasf.f@gmail.com>
2020-04-10 13:32:20 -0300

    Delete the results route

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import Converter from './pages/Converter';
-import Results from './pages/Results';
+
 
 export default function Routes(){
+    //Has a route the the main page, it initialize the app the with the converter page, but it can be changed
+    //We can make as many routes as desired, using this route controller 
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Converter}/>
-                <Route path='/results' exact component={Results}/>
-
             </Switch>   
         </BrowserRouter>
     );
